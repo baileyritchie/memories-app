114b1f96801158607a6a585ffc874877693dcefb
Bailey <baileylritchie@gmail.com>
2020-11-18 20:13:09 -0600

    setting up starter files for client/server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb",extended:true}));
+app.use(bodyParser.urlencoded({limit: "30mb",extended:true}));
+app.use(cors());
+
+const DB_URL = process.env.DB_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(DB_URL,{useNewUrlParser: true,useUnifiedTopology:true})
+  .then(() => app.listen(PORT,() => console.log(`Server running on ${PORT}.`)))
+  .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify',false);
+
+
+
+
+
