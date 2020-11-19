96c44cb5c39e8c0af1d0e9b10711090e8dce623d
Bailey <baileylritchie@gmail.com>
2020-11-19 16:28:19 -0600

    small serverside changes and debugging routes

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -9,29 +9,16 @@ export const getPosts =  async (req,res) => {
     res.status(404).json({message: error.message});
   }
 };
-/* schema
-title: String,
-  message: String, 
-  creator: String,
-  tags: [String],
-  selectedFile: String,
-  likeCount: {
-    type: Number,
-    default: 0
-  },
-  createdAt: {
-    type: Date,
-    default: new Date()
-  }
-*/
+
 export const createPost = async (req,res) => {
   const post = req.body;
   const newPost = new PostMessage(post);
+
   try {
     await newPost.save();
     res.status(201).json(newPost);
-  } catch (error) {
-    res.status(409).json({message: error.message});
+    console.log('post was added')
+  } catch(error) {
+    res.status(409).json({message:error.message});
   }
-
 }
--- server/index.js
+++ server/index.js
@@ -3,13 +3,14 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
 import postRoutes from './routes/posts.js'
 
 dotenv.config();
 const app = express();
 
-app.use(bodyParser.json({limit: "30mb",extended:true}));
-app.use(bodyParser.urlencoded({limit: "30mb",extended:true}));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts',postRoutes);
@@ -20,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(DB_URL,{useNewUrlParser: true,useUnifiedTopology:true})
   .then(() => app.listen(PORT,() => console.log(`Server running on ${PORT}.`)))
-  .catch((error) => console.log(error.message));
+  .catch((error) => console.log('ERROR :',error.message));
 
 mongoose.set('useFindAndModify',false);
 
