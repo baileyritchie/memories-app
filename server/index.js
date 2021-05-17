d0540f31e8a302f290f34be054b97c86988f15a5
Bailey <baileylritchie@gmail.com>
2021-05-17 16:56:09 -0500

    updating mongo for prod

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const DB_URL = process.env.DB_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(DB_URL,{useNewUrlParser: true,useUnifiedTopology:true})
+  .connect(`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@baileyritchie-mongo-production/memories`,{useNewUrlParser: true,useUnifiedTopology:true})
   .then(() => app.listen(PORT,() => console.log(`Server running on ${PORT}.`)))
   .catch((error) => console.log('ERROR :',error.message));
 
