8ad53905eb95fc41741a315fa845899cb6d0043f
Bailey <baileylritchie@gmail.com>
2021-05-17 16:56:23 -0500

    updating api to prod

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL:'http://localhost:5000'});
+const API = axios.create({ baseURL:'https://baileyritchie-memories-frontend.zeet.app/'});
 
 // send token to backend via auth headers
 API.interceptors.request.use((req) => {
