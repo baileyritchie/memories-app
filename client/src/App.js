1359573532f755e3098a7878271ae5bf03970870
Bailey <baileylritchie@gmail.com>
2021-05-17 16:36:39 -0500

    renaming posts file

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,7 @@ import Navbar from '../src/components/Navbar/Navbar'
 import {BrowserRouter,Switch,Route} from 'react-router-dom'
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
-import useStyles from './styles';
 const App = () => {
-  const classes = useStyles();
   return (
     <BrowserRouter>
       <Container maxwidth="lg">
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,7 +3,7 @@ import { Container, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
 import { getPosts } from '../../actions/posts';
-import Posts from '../Posts/Posts';
+import Posts from '../Posts/AllPosts';
 import Form from '../Form/Form';
 
 const Home = () =>  {
