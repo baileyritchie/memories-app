886868ede97717d5a5025e071f3ac1f6420efe80
Bailey <baileylritchie@gmail.com>
2021-05-17 16:10:49 -0500

    fixing more input statements

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React, {useEffect,useState} from 'react'
 import {Container,Grow, Grid} from '@material-ui/core';
-import Posts from '../Posts/Posts.js';
+import Posts from '../Posts/Posts';
 import Form from '../Form/Form.js';
 import {useDispatch} from 'react-redux';
 import {getPosts} from '../..//actions/posts';
